c38d90cb566614fdbffd7c36c01600debdcccb9f
Daan Brouwer <daanbrouwer96@gmail.com>
2023-05-30 14:26:01 +0200

    working V1

--- scripts.js
+++ scripts.js
@@ -1,26 +1,45 @@
 let number = 0;
+let number2 = 0;
 let operator = "";
-function add(a, b) {
-  () => a + b;
-}
-function subtract(a, b) {
-  () => a - b;
-}
-function multiply(a, b) {
-  () => a * b;
-}
-function divide(a, b) {
-  () => a / b;
+
+function numberToInput(btn) {
+  document.getElementById("inputField").value += btn;
 }
 
-function numberpress(btn) {
-  number = btn;
-  return number;
+function operatorpress(btn) {
+  number = parseInt(document.getElementById("inputField").value);
+  btn == "=" ? operate() : (operator = btn);
+  document.getElementById("inputField").value = "";
 }
 
-const operatorpress = (btn) => {
-  operator = btn;
-  console.log(operator);
-};
-
-function operate(operatorpress, a, b) {}
+function operate() {
+  number2 = parseInt(document.getElementById("inputField").value);
+  switch (operator) {
+    case "+":
+      add();
+      break;
+    case "-":
+      subtract();
+      break;
+    case "/":
+      divide();
+      break;
+    case "*":
+      multiply();
+      break;
+    default:
+      break;
+  }
+}
+function add() {
+  document.getElementById("inputField").value = number + number2;
+}
+function subtract() {
+  document.getElementById("inputField").value = number - number2;
+}
+function multiply() {
+  document.getElementById("inputField").value = number * number2;
+}
+function divide() {
+  document.getElementById("inputField").value = number / number2;
+}
